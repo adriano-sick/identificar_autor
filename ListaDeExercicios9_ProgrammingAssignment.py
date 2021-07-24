42a2167554ce0b3d004cb6808b6ece629303914d
Adriano Siqueira <adriano_siqueira_silva@hotmail.com>
2021-07-24 14:38:31 -0300

    7/24/21 2h38PM

--- ListaDeExercicios9_ProgrammingAssignment.py
+++ ListaDeExercicios9_ProgrammingAssignment.py
@@ -2,19 +2,15 @@
 
 import re
 
-def le_assinatura():
-    '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
-    print("Bem-vindo ao detector automático de COH-PIAH.");
-    print("Informe a assinatura típica de um aluno infectado:");
-
-    wal = float(input("Entre o tamanho médio de palavra:"));
-    ttr = float(input("Entre a relação Type-Token:"));
-    hlr = float(input("Entre a Razão Hapax Legomana:"));
-    sal = float(input("Entre o tamanho médio de sentença:"));
-    sac = float(input("Entre a complexidade média da sentença:"));
-    pal = float(input("Entre o tamanho medio de frase:"));
-
-    return [wal, ttr, hlr, sal, sac, pal];
+def main ():
+    '''função para rodar o programa em sua totalidade.''' 
+    print("Bem-vindo ao detector automático de Autoria.");
+    print("Esse algoritmo identifica qual texto tem maior probabilidade\nde ter sido escrito pelo mesmo autor do texto autoral\n");
+    texto_ori = input("Insira o texto autoral: \n");
+    ass_cp = calcula_assinatura(texto_ori);
+    textos = le_textos();
+   
+    return print("O texto", avalia_textos(textos, ass_cp), "possui a assinatura mais proxima ao texto autoral dentre os textos apresentados");
 
 def le_textos():
     '''A funcao le todos os textos a serem comparados e devolve uma lista contendo cada texto como um elemento'''
@@ -153,14 +149,5 @@ def total_letras(texto):
                    letras += len(palavra);
 
     return letras;
-
-
-def main ():
-    '''função para rodar o programa em sua totalidade. Omita para apresentá-la ao corretor automático''' 
-    texto_ori = input("Insira o texto autoral: ");
-    ass_cp = calcula_assinatura(texto_ori);
-    textos = le_textos();
-   
-    return print("O texto", avalia_textos(textos, ass_cp), "possui a assinatura mais proxima ao texto autoral dentre os textos apresentados");
 
 main(); #chamada para a função main
